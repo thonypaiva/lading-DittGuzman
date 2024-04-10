b6359f7f6fdd0e4168940df7e5b51804ec02eb1f
Anthony <adpaguero1992@gmail.com>
2024-04-10 15:59:30 -0500

    se realizo la parte de planes de redes y resultados de gestion de redes sociales dandole algunos estilos y quedando algunos estilos pendientes por hacer se le dio una funcionalidad a las card de los precios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 const menuHambur = document.getElementById('menu');
 const showMenu = document.getElementById('show-menu');
 
+const btnDetalleStandar = document.getElementById('btn--detalles--standar');
+const showDetallesStandar = document.getElementById('estandar');
+const showPriceStandar = document.getElementById('precio--estandar');
+
+const btnDetallesFull = document.getElementById('btn--detalles--full');
+const showDetallesFull = document.getElementById('full');
+const showPriceFull = document.getElementById('precio--full');
+
+const btnDetallesPremium = document.getElementById('btn--detalles--premium');
+const showDetallesPremium= document.getElementById('premium');
+const showPricePremium = document.getElementById('precio--premium');
+
+
+
 menuHambur.addEventListener('click', () => {
     if(showMenu.style.display === 'none' || showMenu.style.display === '' ){
         showMenu.style.display = 'block';
@@ -12,10 +26,35 @@ menuHambur.addEventListener('click', () => {
     }
 })
 
-document.addEventListener('click', function (event){
+document.addEventListener('click',  (event) => {
     if(!showMenu.contains(event.target) && event.target !== menuHambur){
         showMenu.style.display = "none";
         menuHambur.src= './assets/icon-hamburger.svg';
     }
 })
 
+btnDetalleStandar.addEventListener('click', () => {
+    showDetallesStandar.style.display='flex';
+})
+
+showPriceStandar.addEventListener('click', () => {
+    showDetallesStandar.style.display='none';
+})
+
+
+btnDetallesFull.addEventListener('click', () => {
+    showDetallesFull.style.display='flex';
+})
+
+showPriceFull.addEventListener('click', () => {
+    showDetallesFull.style.display='none';
+})
+
+
+btnDetallesPremium.addEventListener('click', () => {
+    showDetallesPremium.style.display='flex';
+})
+
+showPricePremium.addEventListener('click', () => {
+    showDetallesPremium.style.display='none';
+})
